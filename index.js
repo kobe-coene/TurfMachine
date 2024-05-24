57a585141e93c9e2e73fc9716311f3070f17692c
Kobe Coene <kobe.coene@iodigital.com>
2024-05-24 18:09:43 +0200

    Hide variables

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 import { getFirestore, collection, onSnapshot, doc, updateDoc, addDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDszg4tZEyZR-tyiz4h6McvnUhIlgTegLk",
-    authDomain: "turfmachine-c0174.firebaseapp.com",
-    projectId: "turfmachine-c0174",
-    storageBucket: "turfmachine-c0174.appspot.com",
-    messagingSenderId: "79585990175",
-    appId: "1:79585990175:web:261f2619a9a504b5b7272a"
-};
+import firebaseConfig from './firebaseConfig.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
