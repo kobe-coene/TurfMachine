ba49a367fd150452a09452477eb7ad57c046e9c5
Kobe Coene <kobe.coene@iodigital.com>
2024-05-24 20:24:32 +0200

    remove secrets

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDszg4tZEyZR-tyiz4h6McvnUhIlgTegLk",
+    authDomain: "turfmachine-c0174.firebaseapp.com",
+    projectId: "turfmachine-c0174",
+    storageBucket: "turfmachine-c0174.appspot.com",
+    messagingSenderId: "79585990175",
+    appId: "1:79585990175:web:261f2619a9a504b5b7272a"
+};
+
+export default firebaseConfig;
